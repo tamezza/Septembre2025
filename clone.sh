791f8d9a9a7090beb2ffc63638d49f05fb4336c4
Thiziri <thiziri.amezza@cern.ch>
2025-09-09 11:18:59 +0200

    clone script

---
+++ clone.sh
@@ -0,0 +1,7 @@
+echo "# Septembre2025" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/tamezza/Septembre2025.git
+git push -u origin main
